4df387e0fb5331bf17a4dba3338d0528b7d36abe
Shahin <69809384+SahinBabazada@users.noreply.github.com>
2024-12-17 16:06:33 +0400

    Update app.py

--- app.py
+++ app.py
@@ -41,5 +41,5 @@ def generate_qr():
 
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get('PORT', 5000))  # Use the PORT environment variable
+    port = int(os.environ.get('PORT', 8000))  # Use the PORT environment variable
     app.run(host='0.0.0.0', port=port, debug=True)
